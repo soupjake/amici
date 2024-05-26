f3b51e466e5b6f7793d406d50c94a9deb3b0d7c6
soupjake <super.jake@hotmail.co.uk>
2024-05-26 21:09:16 +0100

    added map image

--- src/ui/friend/FriendInfo.tsx
+++ src/ui/friend/FriendInfo.tsx
@@ -7,6 +7,7 @@ import {
 } from '@ant-design/icons'
 import { Flex, Rate, Typography } from 'antd'
 
+import map from '../../assets/map.jpg'
 import { Friend } from '../../types/friend'
 import { FriendOnline } from './FriendOnline'
 
@@ -78,6 +79,16 @@ export const FriendInfo = (props: Props) => {
                     ))}
                 </>
             ) : null}
+            <img className="friend-map" src={map} />
+            <Text className="friend-subtext">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Text>
         </Flex>
     )
 }
--- src/ui/friend/FriendOnline.tsx
+++ src/ui/friend/FriendOnline.tsx
@@ -11,7 +11,7 @@ export const FriendOnline = (props: Props) => {
             width="10"
             xmlns="http://www.w3.org/2000/svg"
             className="friend-online">
-            <circle r="5" cx="5" cy="5" fill={online ? '#73d13d' : '#ff4d4f'} />
+            <circle r="5" cx="5" cy="5" fill={online ? '#389e0d' : '#cf1322'} />
         </svg>
     )
 }
